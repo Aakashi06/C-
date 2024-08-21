8689c18725ae47180d74f4818af3db3c3fc097e8
Aakashi Jaiswal <144087566+Aakashi06@users.noreply.github.com>
2024-08-21 15:10:41 +0530

    Add files via upload

---
+++ datatype.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //int datatype
+    int apples = 5 ;
+    cout<<apples<<endl;
+    cout<<"size of int: "<<sizeof(apples)<<endl;
+
+    //char datatype
+    char s ='A';
+    cout<<s<<endl;
+    cout<<"size of s: "<<sizeof(s)<<endl;
+
+    //Bool datatype
+    bool f =true;
+    cout<<f<<endl;
+    cout<<"size of bool:"<<sizeof(f)<<endl;
+
+    //float datatype
+    float pi = 1.5;
+    cout<<pi<<endl;
+    cout<<"size of float: "<<sizeof(pi)<<endl;
+
+}
